a597717914d7ef02fa65212cbf37c557a2c70757
suchothendav <suchothendav@paypal.com>
2016-10-20 13:05:31 -0700

    Build the User class

---
+++ client/app/user/user.ts
@@ -0,0 +1,10 @@
+export class User {
+    id: number;
+    avatar_url: string;
+    html_url:string;
+    company: string;
+    location: string;
+    blog: string;
+    login: string;
+    name:string;
+}
