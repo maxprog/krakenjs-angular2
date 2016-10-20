e611efd150e65d65e602ca485db7bfd4c900efb1
suchothendav <suchothendav@paypal.com>
2016-10-20 00:16:11 -0700

    turn off uglify plugin for now

--- client/polyfills.ts
+++ client/polyfills.ts
@@ -1,2 +1,12 @@
 require('zone.js/dist/zone');
 import 'reflect-metadata';
+
+// import { enableProdMode } from '@angular/core';
+//
+// let nodeEnv = process.env.NODE_ENV || 'development';
+//
+// if (/^dev/i.test(nodeEnv)) {
+//     require('zone.js/dist/long-stack-trace-zone');
+// } else {
+//     enableProdMode();
+// }
--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,13 @@ const Webpack = require('webpack');
 const WebpackMerge = require('webpack-merge');
 const CommonConfig = require('./webpack-util/webpack.common');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
 
 module.exports = WebpackMerge(CommonConfig, {
     entry: {
         vendor: './client/vendor.ts',
         polyfills: './client/polyfills.ts',
-        main: './client/app/main.ts'
+        app: './client/app/main.ts'
     },
     output: {
         filename: './bundle/[name].js'
@@ -16,11 +17,11 @@ module.exports = WebpackMerge(CommonConfig, {
         new ExtractTextPlugin('./bundle/[name].css'),
         new Webpack.NoErrorsPlugin(),
         new Webpack.optimize.DedupePlugin(),
-        new Webpack.optimize.UglifyJsPlugin({
-            mangle: {
-                keep_fnames: true
-            }
-        }),
+        // new Webpack.optimize.UglifyJsPlugin({
+        //     mangle: {
+        //         keep_fnames: true
+        //     }
+        // }),
         new Webpack.DefinePlugin({
             'process.env': {
                 'ENV': JSON.stringify(ENV)
