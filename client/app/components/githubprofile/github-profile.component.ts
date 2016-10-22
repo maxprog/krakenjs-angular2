c65e974dfc507d40d6afa23ff8d73dcd16a83d28
suchothendav <suchothendav@paypal.com>
2016-10-21 17:04:47 -0700

    safe guard operator

--- client/app/components/githubprofile/github-profile.component.ts
+++ client/app/components/githubprofile/github-profile.component.ts
@@ -10,12 +10,12 @@ import { Subject } from 'rxjs/Subject';
     selector: 'githubprofile',
     template: `<div *ngFor="let userData of user | async" class="profile">
         <div class="prf-pdng" *ngIf="userData.avatar_url">
-            <a href="{{userData.html_url}}">
-                <img src="{{userData.avatar_url}}" class="center-block img-responsive img-thumbnail" alt="{{userData.name}}" width="200px"/>
+            <a href="{{userData?.html_url}}">
+                <img src="{{userData?.avatar_url}}" class="center-block img-responsive img-thumbnail" alt="{{userData?.name}}" width="200px"/>
             </a>
         </div>
-        <h3 *ngIf="userData.name" class="text-center">{{userData.name}}</h3>
-        <button *ngIf="userData.login && !profilePage" (click)="onClick(userData)" type="button" class="center-block btn btn-primary marg">{{userData.inmemory? "Edit Profile" : "Build Profile"}}</button>
+        <h3 *ngIf="userData.name" class="text-center">{{userData?.name}}</h3>
+        <button *ngIf="userData.login && !profilePage" (click)="onClick(userData)" type="button" class="center-block btn btn-primary marg">{{userData?.inmemory ? "Edit Profile" : "Build Profile"}}</button>
         <div *ngIf="profilePage" class="prf-pdng">
 
         </div>
